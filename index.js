5e7d4806ed9c727cad2b0debfc9234429c95372b
koebe1 <koe.be.pay@web.de>
2021-11-09 10:49:28 +0100

    simple form validation rehearsal

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("this works!")
+const form = document.getElementById("email-form");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const error = document.getElementById("error");
+
+console.log(password.value);
+
+const passwordValidation = e => {
+  const errMsg = "Please enter a password!";
+  if (password.value === "" || password.value === null) {
+    error.innerHTML = errMsg;
+  }
+};
+
+form.addEventListener("submit", e => {
+  const errMsg = "Please enter a password!";
+  if (password.value === "" || password.value === null) {
+    error.innerHTML = errMsg;
+    e.preventDefault();
+  }
+});
